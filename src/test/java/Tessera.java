5336576ae95e014beafd1bdbf56481263993c77f
Francesco Proietti <francesco.proietti.37@gmail.com>
2026-03-31 09:18:43 +0200

    tessera

---
+++ src/test/java/Tessera.java
@@ -0,0 +1,45 @@
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tessere")
+public class Tessera {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "numero_tessera")
+    private Long numeroTessera;
+
+    @Column(name = "data_emissione")
+    private LocalDate dataEmissione;
+
+    @Column(name = "data_scadenza")
+    private LocalDate dataScadenza;
+
+    @Column(name = "codice_fiscale")
+    private String codiceFiscale;
+
+
+
+    public Tessera() {
+    }
+
+
+    public Tessera(Long numeroTessera, LocalDate dataEmissione, LocalDate dataScadenza, String codiceFiscale) {
+        this.numeroTessera = numeroTessera;
+        this.dataEmissione = dataEmissione;
+        this.dataScadenza = dataScadenza;
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+}
