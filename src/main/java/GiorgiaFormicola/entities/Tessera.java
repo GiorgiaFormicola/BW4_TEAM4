ce80d7b343019db9aacd03f8bf870ab825c9831b
Lynliash <francesco.alves1408@gmail.com>
2026-04-02 10:21:15 +0200

    Fix

--- src/main/java/GiorgiaFormicola/entities/Tessera.java
+++ src/main/java/GiorgiaFormicola/entities/Tessera.java
@@ -36,6 +36,9 @@ public class Tessera {
         this.utente = utente;
     }
 
+    public Tessera(long numeroTessera) {
+    }
+
     public Long getNumeroTessera() {
         return numeroTessera;
     }
